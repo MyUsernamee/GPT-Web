aa357e396f4062baffd31b0c8fbc4b4dec752927
MyUsername <delanoleslieprof@gmail.com>
2023-04-06 12:49:26 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import selenium
+import openai
+
+openai.api_key = ""
+
+# Selenium Imports
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
